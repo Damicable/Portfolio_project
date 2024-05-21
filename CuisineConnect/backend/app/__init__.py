1b59962bf9786e3f81eed3910bad0e42281aaa8d
HarpLoid <walesii19@gmail.com>
2024-05-21 15:55:11 +0100

    created backend branch

--- CuisineConnect/backend/app/__init__.py
+++ CuisineConnect/backend/app/__init__.py
@@ -10,6 +10,8 @@ def create_app():
     app = Flask(__name__)
     
     app.config.from_object(Config)
-    db.init_app(app)
+    
+    with app.app_context():
+        db.init_app(app)
     
     return app
