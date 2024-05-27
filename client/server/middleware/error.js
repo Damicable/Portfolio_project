0eb3f1f389e8f72b7beb1179ed14806a31be5ee5
amineladraa1 <amineladraa67@gmail.com>
2024-05-27 15:35:40 +0100

    deleted server

--- client/server/middleware/error.js
+++
@@ -1,6 +0,0 @@
-const { log } = require('../startup/logger');
-
-module.exports = (err, req, res, next) => {
-  log.error(err, err.message);
-  res.status(500).send(`There has been an error: ${err.message}`);
-};
--- client/server/startup/logger.js
+++
@@ -1,4 +0,0 @@
-const pino = require('pino');
-const log = pino({ prettyPrint: { colorize: true } });
-
-module.exports.log = log;
