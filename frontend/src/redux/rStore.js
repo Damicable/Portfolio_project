86b4fcda08868680756b765a904c88351563c20f
HarpLoid <walesii19@gmail.com>
2024-05-30 13:13:09 +0100

    removed frontend to merge new frontend

--- frontend/src/redux/rStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk';
-import recipeReducer from './recipeReducer';
-
-const store = createStore(recipeReducer, applyMiddleware(thunk));
-
-export default store;
--- frontend/src/vars.js
+++
@@ -1,8 +0,0 @@
-const vars = {
-  accent: "#DC2526",
-  text: "#353333",
-  shadow: "#C4C4C4",
-  background: "#EFE8E9",
-};
-
-export default vars;
